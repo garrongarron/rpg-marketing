31336d1c12ac9bd9f1e18f6f7322a7e735b2dcda
Samu Garron <garron.garron@gmail.com>
2021-10-07 21:02:49 -0300

    Dragon escupe fuego

--- src/basic/Camera.js
+++ src/basic/Camera.js
@@ -1,3 +1,3 @@
-const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.01, 10000 );
+const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
 export default camera
